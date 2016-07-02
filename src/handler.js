33deaa86944507b9d9e53233ddffb19eed1318a0
bradreeder <bradjohnreeder@gmail.com>
2016-07-02 15:19:36 +0100

    set up css file

--- src/handler.js
+++ src/handler.js
@@ -7,6 +7,12 @@ function handler(req, res) {
       res.writeHead(200, { 'Content-type': 'text/html' });
       res.end(data);
     });
+  } else if (url.includes('public')) {
+    const ext = url.split('.')[1];
+    fs.readFile(`${__dirname}/..${url}`, (err, data) => {
+      res.writeHead(200, { 'Content-type': `text/${ext}` });
+      res.end(data);
+    });
   } else {
     res.writeHead(404);
     res.end('<h1>404 -- Page requested cannot be found</h1>');
