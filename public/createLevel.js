9c741f2ccd6f6faa90cc931c3b72a88d9a8e892c
bradreeder <bradjohnreeder@gmail.com>
2016-07-03 13:06:17 +0100

    pushing changes github didnt acknowledge

--- public/createLevel.js
+++ public/createLevel.js
@@ -1,5 +1,5 @@
 const gameContainer = document.getElementById('game-container');
-let levelMap = []; let currentLevel = ''; let currentPosition = []; // eslint-disable-line
+let levelMap = []; let currentLevel = 0; let currentPosition = []; let tilesLeftToExplore = 0; // eslint-disable-line
 
 const createLevel = (function() {  // eslint-disable-line
   function generateLevel (level, levelNo) { // eslint-disable-line
@@ -8,13 +8,15 @@ const createLevel = (function() {  // eslint-disable-line
       row.forEach((item, itemIndex) => {
         const gridBox = document.createElement('div');
         gridBox.className = `col-1-${row.length} ${item}`;
-        console.log(rowIndex, itemIndex);
         gridBox.id = `${JSON.stringify(rowIndex) + JSON.stringify(itemIndex)}`;
         gameContainer.appendChild(gridBox);
         if (item === 'active') { // if id char ever has 4 characters then refactor
           currentPosition = gridBox.id.length === 2
             ? [+(gridBox.id.charAt(0)), +(gridBox.id.charAt(1))]
             : [+(gridBox.id.charAt(0)), +(gridBox.id.charAt(1) + gridBox.id.charAt(2))];
+        }
+        if (item === 'unexplored') {
+          tilesLeftToExplore++;
         }
       });
     });
