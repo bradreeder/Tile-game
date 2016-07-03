e73c8591de06902bd0b981e092e907b0482be3fe
bradreeder <bradjohnreeder@gmail.com>
2016-07-03 03:44:38 +0100

    fixed bugs so that all maps work

--- public/createLevel.js
+++ public/createLevel.js
@@ -8,10 +8,13 @@ const createLevel = (function() {  // eslint-disable-line
       row.forEach((item, itemIndex) => {
         const gridBox = document.createElement('div');
         gridBox.className = `col-1-${row.length} ${item}`;
+        console.log(rowIndex, itemIndex);
         gridBox.id = `${JSON.stringify(rowIndex) + JSON.stringify(itemIndex)}`;
         gameContainer.appendChild(gridBox);
-        if (item === 'active') {
-          currentPosition = [+(gridBox.id.charAt(0)), +(gridBox.id.charAt(1))];
+        if (item === 'active') { // if id char ever has 4 characters then refactor
+          currentPosition = gridBox.id.length === 2
+            ? [+(gridBox.id.charAt(0)), +(gridBox.id.charAt(1))]
+            : [+(gridBox.id.charAt(0)), +(gridBox.id.charAt(1) + gridBox.id.charAt(2))];
         }
       });
     });
